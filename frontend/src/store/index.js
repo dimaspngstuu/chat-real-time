6e98d1d1b5d25754577520b4b5adfae161e0b9ab
dimaspngstuu123@gmail.com <dimaspngstuu123@gmail.com>
2023-09-09 07:51:00 +0700

    added a store redux

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,1 +1,19 @@
-import {createStore,compose,combineReducers,applyMiddleware} from 'redux'
+import {legacy_createStore,compose,combineReducers,applyMiddleware} from 'redux';
+
+import thunkMiddleware from "redux-thunk";
+
+
+//create a reducer
+const rootReducer = combineReducers({
+
+});
+
+const middleware = [thunkMiddleware];
+
+
+const store = legacy_createStore(rootReducer,compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
+
